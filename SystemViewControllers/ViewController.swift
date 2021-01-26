2c1d725541407d5927f95b542ad9f2c9b291d74e
gsbaker <gsbaker2000@icloud.com>
2021-01-26 17:24:04 +0000

    Added share functionality.

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Actions
+    @IBAction func shareButtonTapped(_ sender: Any) {
+        guard let image = imageView.image else {
+            return
+        }
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender as? UIView
+        
+        present(activityController, animated: true, completion: nil)
+    }
+    
 
-
+    @IBAction func safariButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: Any) {
+    }
 }
 
