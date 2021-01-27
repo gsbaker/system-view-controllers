f10631b3441bcdb88575e14c2767ef01e40422de
gsbaker <gsbaker2000@icloud.com>
2021-01-27 10:36:05 +0000

    Added Safari and Alert View Controllers

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
     
@@ -31,9 +32,31 @@ class ViewController: UIViewController {
     
 
     @IBAction func safariButtonTapped(_ sender: Any) {
+        if let url = URL(string: "https://www.apple.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+            
+        }
     }
     
     @IBAction func cameraButtonTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in print("User selected Camera action")
+        })
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in print("User selected Photo Library action")
+        })
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.popoverPresentationController?.sourceView = sender as? UIView
+        
+        present(alertController, animated: true, completion: nil)
+        
     }
     
     @IBAction func emailButtonTapped(_ sender: Any) {
